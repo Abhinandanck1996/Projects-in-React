c9c0f312b858dbcd386cb4ba79f5111e452ac61d
Abhinandan CK <abhinandanck1996@gmail.com>
2022-04-21 15:05:10 +0530

    Projects in React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 
+import "./components/global.css";
+import SpotifyLogin from './Spotify Component/SpotifyLogin';
+import EmployeeForm from './Employee/EmployeeForm';
+import Promismethod from './Employee/Promismethod';
+import Pagination from './Employee/Pagination';
+
+
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ <Pagination/>
     </div>
   );
 }
 
 export default App;
+
+
